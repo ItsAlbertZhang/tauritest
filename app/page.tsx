4ac6426e8508f12a4bd29f481f15d20a7017eca0
ItsAlbertZhang <itsalbertzhang@gmail.com>
2024-02-19 23:52:00 +0800

    0.1.6

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     return (
         <div>
             <p>Hello, tauri!</p>
-            <p>Hello, 0.1.5!</p>
+            <p>Hello, 0.1.6!</p>
             <>{`当前版本: ${version}`}</>
         </div>
     );
